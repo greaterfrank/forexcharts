8cd13b6214f33f1e580585d9627f505328e8457d
greaterfrank <89160354+greaterfrank@users.noreply.github.com>
2022-04-15 12:16:02 -0400

    add server

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
 # note_golang_restful_api
 
-initialization: 
-go env -w GO111MODULE=on
-go mod init golang.frank/notes/note_golang_restful_api
-go mod tidy
+### initialization: 
+```
+    go env -w GO111MODULE=on
+    go mod init golang.frank/notes/note_golang_restful_api
+    go mod tidy
+```
+
+### run:
+```
+    go run main.go
+    curl http://192.168.1.103:8080/api/data/getItem?id=2
+```
 
 
 
--- api/main.go
+++ api/main.go
@@ -2,8 +2,74 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
+
+type Settings struct {
+	Port string `json:"port"`
+}
+
+type Response struct {
+	Error   int    `json:"Error"`
+	Message string `json:"Message"`
+	Data    string `json:"Data"`
+}
+
+func handleGetItem() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		fmt.Println("handleGetItem")
+		id := c.DefaultQuery("id", "")
+		var response Response
+		response.Error = 0
+		response.Data = id
+		response.Message = "ok"
+		c.JSON(http.StatusOK, response)
+	}
+}
+
+func handleGetRoot() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		fmt.Println("handleGetRoot")
+		var response Response
+		response.Error = 0
+		response.Data = "root"
+		response.Message = "ok"
+		c.JSON(http.StatusOK, response)
+	}
+}
+
+func initRouter() *gin.Engine {
+	engine := gin.New()
+	engine.UseRawPath = true
+	engine.ForwardedByClientIP = true
+
+	engine.Use(cors.New(cors.Config{
+		AllowAllOrigins: true,
+		AllowMethods:    []string{"GET", "POST"},
+		AllowHeaders:    []string{"*"},
+	}))
+
+	data := engine.Group("/api/data")
+	//data.POST("/updateItem", handlePOST())
+	data.GET("/getItem", handleGetItem())
+	data.GET("/", handleGetRoot())
+
+	return engine
+}
 
 func main() {
 	fmt.Println("Hello Golang !")
+	conf := Settings{Port: "8080"}
+
+	engine := initRouter()
+
+	server := &http.Server{
+		Addr:    ":" + conf.Port,
+		Handler: engine,
+	}
+
+	server.ListenAndServe()
 }
